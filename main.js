ddbc71af6623e010924dd388fed843c08cbad6be
Aashish Yadav <yadavaashish276@gmail.com>
2024-09-09 01:59:52 +0530

    learning fileSystem

---
+++ main.js
@@ -0,0 +1,4 @@
+// const b = require("./script");
+// console.log(b);
+const fs = require("fs");
+fs.writeFileSync("./hello.txt", "Hello there");
---
+++ script.js
@@ -0,0 +1,18 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+// app.get('/profile', function (req, res) {
+//     res.send('Hello Profile')
+//   })
+//   app.get('/contact', function (req, res) {
+//     res.send('Hello Contact')
+//   })
+
+// app.listen(3000)
+
+let a = 20;
+console.log(a);
+module.exports = a;
